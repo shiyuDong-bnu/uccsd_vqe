e61faf8428d006dbf43a5b5c362e25b7c122a404
Dong-Shiyu-NJU <2656304384@qq.com>
2025-01-11 20:57:07 +0800

    test energy function using HF state

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Implementation of ucc vqe using cirq and openfermion
 - [x] get hamiltonian
-- [ ] get qubits number
 - [ ] get uccsd ansatz
-- [ ] get energy and gradient
+- [x] get  energy
+- [ ] get  gradient
 - [ ] optimization
---
+++ energy.py
@@ -0,0 +1,18 @@
+import cirq
+def get_energy(circuit,jw_hamiltonian,qubits):
+    energy=0
+    simulator=cirq.Simulator()
+    for key in jw_hamiltonian.terms:
+        if len(key)==0:
+            energy+=jw_hamiltonian.terms[key]
+        else:
+            measure_ansatz=circuit.copy()
+            each_ops=[eval("cirq."+ops[1])(qubits[ops[0]]) for ops in key]
+            total_op=1
+            for op in each_ops:
+                total_op*=op
+            ev_list=simulator.simulate_expectation_values(
+                measure_ansatz,observables=[total_op]
+            )
+            energy+=ev_list[0]*jw_hamiltonian.terms[key]
+    return energy
